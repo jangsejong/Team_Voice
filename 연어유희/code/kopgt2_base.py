2aa0e97fc720a06891de299e972c1e4c00d183f1
SEJONG JANG <92250663+jangsejong@users.noreply.github.com>
2023-03-03 10:05:03 +0700

    Update kopgt2_base.py

--- 연어유희/code/kopgt2_base.py
+++ 연어유희/code/kopgt2_base.py
@@ -1,6 +1,5 @@
 import torch
 from transformers import GPT2LMHeadModel
-
 from transformers import PreTrainedTokenizerFast
 tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2", bos_token='</s>', eos_token='</s>', unk_token='<unk>', pad_token='<pad>', mask_token='<mask>') 
 tokenizer.tokenize("안녕하세요. 한국어 GPT-2 입니다.😤:)l^o")
